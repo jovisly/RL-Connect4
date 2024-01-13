7fd232b7489bbef35c23df12679c77fc3bff998c
jovisly <j@ovisly.com>
2024-01-13 13:37:58 +0800

    qdn update

--- opponents/opponent_dqn.py
+++ opponents/opponent_dqn.py
@@ -1,5 +1,7 @@
 import torch
 
+from utils import get_available_col_nums
+
 from learners.learner_dqn import DQN
 from learners.learner_utils import convert_positions_to_board
 
@@ -11,11 +13,25 @@ def get_move(positions, player):
     state = convert_positions_to_board(positions, player)
     state_tensor = torch.tensor(state, dtype=torch.float32)
 
+    available_cols = get_available_col_nums(positions)
     with torch.no_grad():
         q_values = model(state_tensor)
-        # Plus 1 because we are 1 to 7.
-        action = torch.argmax(q_values).item() + 1
+        q_values_list = q_values.squeeze().tolist()
+        q_dict = {
+            i + 1: q
+            for i, q in enumerate(q_values_list)
+            if i + 1 in available_cols
+        }
+        # Take the key with the biggest value.
+        action = max(q_dict, key=q_dict.get)
 
     return action
 
 
+
+if __name__ == "__main__":
+    positions = {"P1": [(1, 4)], "P2": []}
+    player = "P2"
+
+    move = get_move(positions, player)
+    print(move)
